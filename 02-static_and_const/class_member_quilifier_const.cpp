4cd8a3386d4a0ad6e73a382addce2b5c507eb8d6
Chong Chen <chongchen1999@gmail.com>
2024-08-05 09:33:58 -0400

    update files

---
+++ 02-static_and_const/class_member_quilifier_const.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+class MyClass {
+public:
+
+    // implicitly add: MyClass *const this
+    void printValue() {
+        std::cout << "call printValue()" << std::endl;
+        std::cout << value << std::endl; // 实际上是 this->value
+        std::cout << typeid(this).name() << std::endl; // 打印 this 的类型
+    }
+
+    // implicitly add: const MyClass *const this
+    void printValue() const {
+        std::cout << "call printValue() const" << std::endl; 
+        std::cout << value << std::endl; // 实际上是 this->value
+        std::cout << typeid(this).name() << std::endl; // 打印 this 的类型
+    }
+
+    void foo() {
+        std::cout << "call printValue() const" << std::endl; 
+        std::cout << value << std::endl; // 实际上是 this->value
+        std::cout << typeid(this).name() << std::endl; // 打印 this 的类型
+    }
+
+
+
+private:
+    int value = 41;
+    const int constValue = 42;
+};
+
+int main() {
+    const auto myClass = MyClass();
+    auto myClass2 = MyClass();
+
+    myClass.printValue();
+    myClass2.printValue();
+
+    myClass.foo();
+
+    return 0;
+}
